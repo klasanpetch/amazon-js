cb88b6a3fc17185400a38653b124b9ffb0daa8a6
klasanpetch <tumhanry383@gmail.com>
2025-10-04 16:00:03 +0700

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -23,7 +23,8 @@ export function addToCart(productId) {
         if (productId === cartItem.productId){
             matchingItem = cartItem;
         }
-    })
+    });
+
     if (matchingItem) {
         matchingItem.quantity += 1;
     } else{
@@ -47,5 +48,18 @@ export function removeFromCart(productId){
 
     cart = newCart;
 
+    saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((cartItem)=>{
+        if (productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
