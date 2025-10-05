7730c8e9723e46e27c2a31318587ec6f4b63c57a
klasanpetch <tumhanry383@gmail.com>
2025-10-05 23:09:50 +0700

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSumary.js"
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
