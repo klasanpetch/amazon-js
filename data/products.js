9379d0c46fbd840f7831fa583390d771ce533c36
klasanpetch <tumhanry383@gmail.com>
2025-10-09 21:30:41 +0700

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -84,6 +84,28 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+      'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productData) => {
+    products = productData.map((productDetails) => {
+      if (productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+  return promise;
+}
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+
+/*
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -93,13 +115,14 @@ export function loadProducts(fun) {
         return new Clothing(productDetails);
       }
       return new Product(productDetails);
-    });;
+    });
     console.log('load products');
     fun();
   });
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+*/
 
 /*
 export const products = [
